ef3acaada92a52669faec4e41c1fa6b01f142628
MJJH <martijnvriens94@live.nl>
2015-04-08 12:49:16 +0200

    test

---
+++ ICT4Events_Group1/ICT4Events_Group1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events_Group1
+{
+    class Class1
+    {
+        Boolean werkt = false;
+    }
+}
