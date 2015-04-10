53943fe3614b605ca0f4bd3afe8cf3eefb786eba
kevin1995deman <kevin1995deman@gmail.com>
2015-04-10 15:41:26 +0100

    img delt

--- ICT4Events_Group1/ICT4Events_Group1/Properties/Resources.Designer.cs
+++ ICT4Events_Group1/ICT4Events_Group1/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ICT4Events_Group1.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ict4events_os_home {
-            get {
-                object obj = ResourceManager.GetObject("ict4events_os_home", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
