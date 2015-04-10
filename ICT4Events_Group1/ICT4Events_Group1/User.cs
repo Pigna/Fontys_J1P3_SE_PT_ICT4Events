e201a37a1bc657202e31c9fe4f2956c0e8a47cb3
kevin1995deman <kevin1995deman@gmail.com>
2015-04-10 10:55:27 +0100

    user class properties & fields

--- ICT4Events_Group1/ICT4Events_Group1/User.cs
+++ ICT4Events_Group1/ICT4Events_Group1/User.cs
@@ -8,5 +8,26 @@ namespace ICT4Events_Group1
 {
     class User
     {
+        private string username;
+        private string RFID;
+        List<Item> itemlist = new List<Item>();
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public User(string username, string rfid)
+        {
+            this.username = username;
+            this.RFID = rfid;
+        }
+        public bool additem(int itemid)
+        {
+            
+            return true;
+        }
+
     }
 }
