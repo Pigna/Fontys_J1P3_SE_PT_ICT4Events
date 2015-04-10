850635ec1531c35b7526ee33d06f2b71463eea29
kevin1995deman <kevin1995deman@gmail.com>
2015-04-10 13:53:09 +0100

    changes message

--- ICT4Events_Group1/ICT4Events_Group1/Message.cs
+++ ICT4Events_Group1/ICT4Events_Group1/Message.cs
@@ -8,5 +8,13 @@ namespace ICT4Events_Group1
 {
     class Message
     {
+        List<Message> commentlist = new List<Message>();
+        List<User> liked = new List<User>();
+        List<Report> reported = new List<Report>();
+        private User sent;
+        private string title;
+        private string content;
+        private string attachment; // file
+        
     }
 }
