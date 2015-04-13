80bc6533071fd29946dcb0a0942f0fb5b1e8f07e
Pigna <myron.antonissen@gmail.com>
2015-04-13 11:57:59 +0200

    EventBeheer + Toevoeg | GebruikerBeheer + Toevoeg | UserDatabase Changes

--- ICT4Events_Group1/ICT4Events_Group1/UserDatabase.cs
+++ ICT4Events_Group1/ICT4Events_Group1/UserDatabase.cs
@@ -11,6 +11,35 @@ namespace ICT4Events_Group1
         //methodes
         public bool adduser(User user)
         {
+//connect!
+            /*
+            try
+            {
+                
+                //This is the mysql command that we will query into the db.
+                //It uses Prepared statements and the Placeholder is @name.
+                //Using prepared statements is faster and secure.
+                String cmdText = "INSERT INTO Gebruiker(Id, Event, Voornaam, Tussenvoegsel, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Paspoort, Rekeningnr, Foto, Email, Telefoon, Mobiel, Username, Password, Startdatum, Einddatum, Betaald) VALUES(@Id, @Event, @Voornaam, @Tussenvoegsel, @Achternaam, @Straat, @Huisnummer, @Postcode, @Woonplaats, @Paspoort, @Rekeningnr, @Foto, @Email, @Telefoon, @Mobiel, @Username, @Password, @Startdatum, @Einddatum, @Betaald)";
+                MySqlCommand cmd = new MySqlCommand(cmdText, con);
+                cmd.Prepare();
+                //we will bound a value to the placeholder
+                //Id, Event, Voornaam, Tussenvoegsel, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Paspoort, Rekeningnr, Foto, Email, Telefoon, Mobiel, Username, Password, Startdatum, Einddatum, Betaald
+                cmd.Parameters.AddWithValue("@vorm", figuur.vorm);
+                cmd.Parameters.AddWithValue("@x", figuur.x);
+                cmd.Parameters.AddWithValue("@y", figuur.y);
+                cmd.Parameters.AddWithValue("@w", figuur.w);
+                cmd.Parameters.AddWithValue("@h", figuur.h);
+                cmd.ExecuteNonQuery(); //execute the mysql command
+            }
+            catch (MySqlException err)
+            {
+                Console.WriteLine("Error: " + err.ToString());
+            }
+            finally
+            {
+                Close();
+            } //remember to close the connection after accessing the database
+            */
             return true;
         }
 
