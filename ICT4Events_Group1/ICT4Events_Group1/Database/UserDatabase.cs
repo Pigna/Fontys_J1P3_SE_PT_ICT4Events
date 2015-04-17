f6b70424aeae4dab3887f75f383221c71ad33d26
Pigna <myron.antonissen@gmail.com>
2015-04-17 11:54:16 +0200

    User toevoegen - toevoegen in db

--- ICT4Events_Group1/ICT4Events_Group1/Database/UserDatabase.cs
+++ ICT4Events_Group1/ICT4Events_Group1/Database/UserDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Oracle.DataAccess.Client;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,41 +7,14 @@ using System.Threading.Tasks;
 
 namespace ICT4Events_Group1
 {
-    class UserDatabase
+    class UserDatabase : Database
     {
         //methodes
-        public bool adduser(User user)
+        public bool adduser(int id, Event evnt, string voornaam, string tussenvoegsel, string achternaam, string straat, string huisnummer, string postcode, string woonplaats, string paspoort, string rekeningnr, string foto, string email, string telefoon, string mobiel, string username, string password, DateTime date1, DateTime date2, bool betaald)
         {
-//connect!
-            /*
-            try
-            {
-                
-                //This is the mysql command that we will query into the db.
-                //It uses Prepared statements and the Placeholder is @name.
-                //Using prepared statements is faster and secure.
-                String cmdText = "INSERT INTO Gebruiker(Id, Event, Voornaam, Tussenvoegsel, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Paspoort, Rekeningnr, Foto, Email, Telefoon, Mobiel, Username, Password, Startdatum, Einddatum, Betaald) VALUES(@Id, @Event, @Voornaam, @Tussenvoegsel, @Achternaam, @Straat, @Huisnummer, @Postcode, @Woonplaats, @Paspoort, @Rekeningnr, @Foto, @Email, @Telefoon, @Mobiel, @Username, @Password, @Startdatum, @Einddatum, @Betaald)";
-                MySqlCommand cmd = new MySqlCommand(cmdText, con);
-                cmd.Prepare();
-                //we will bound a value to the placeholder
-                //Id, Event, Voornaam, Tussenvoegsel, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Paspoort, Rekeningnr, Foto, Email, Telefoon, Mobiel, Username, Password, Startdatum, Einddatum, Betaald
-                cmd.Parameters.AddWithValue("@vorm", figuur.vorm);
-                cmd.Parameters.AddWithValue("@x", figuur.x);
-                cmd.Parameters.AddWithValue("@y", figuur.y);
-                cmd.Parameters.AddWithValue("@w", figuur.w);
-                cmd.Parameters.AddWithValue("@h", figuur.h);
-                cmd.ExecuteNonQuery(); //execute the mysql command
-            }
-            catch (MySqlException err)
-            {
-                Console.WriteLine("Error: " + err.ToString());
-            }
-            finally
-            {
-                Close();
-            } //remember to close the connection after accessing the database
-            */
-            return true;
+            //connect!
+            return doQuery("INSERT INTO Gebruiker (Id, Event, Voornaam, Tussenvoegsel, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Paspoort, Rekeningnr, Foto, Email, Telefoon, Mobiel, Username, Password, RFID, Startdatum, Einddatum, Betaald)" +
+                           "VALUES (" + id + ", " + evnt.id + ", " + voornaam + ", " + tussenvoegsel + ", " + achternaam + ", " + straat + ", " + huisnummer + ", " + postcode + ", " + woonplaats + ", " + paspoort + ", " + rekeningnr + ", " + foto + ", " + email + ", " + telefoon + ", " + mobiel + ", " + username + ", " + password + ", NULL, " + date1 + ", " + date2 + ", " + betaald +");") > 0;
         }
 
         public bool modifyUser(User newuser, User olduser)
@@ -54,12 +28,43 @@ namespace ICT4Events_Group1
         }
         public List<User> getUserlist()
         {
-            string code = "SELECT * FROM GEBRUIKER;";
-            return null;
+            List<User> ret = new List<User>();
+
+            OracleDataReader data = getQuery("SELECT Id, Username, Voornaam, Tussenvoegsel, Achternaam FROM Gebruiker;");
+
+            while (data.Read())
+            {
+                ret.Add(new User(data.GetInt16(0), data.GetString(1), data.GetString(2), data.GetString(3), data.GetString(4)));
+            }
+            return ret;
         }
         public void getDataRFID(RFID rfid)
         {
-            string code = "SELECT * FROM GEBRUIKER WHERE RFID = " + rfid.Code + ";";
+            //string code = "SELECT * FROM GEBRUIKER WHERE RFID = " + rfid.Code + ";";
+        }
+        public bool usernameExist(string username)
+        {
+            OracleDataReader data = getQuery("SELECT Id FROM Gebruiker WHERE Username = " + username + ";");
+            if (data.FieldCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool emailExist(string email)
+        {
+            OracleDataReader data = getQuery("SELECT Id FROM Gebruiker WHERE Email = " + email + ";");
+            if (data.FieldCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
