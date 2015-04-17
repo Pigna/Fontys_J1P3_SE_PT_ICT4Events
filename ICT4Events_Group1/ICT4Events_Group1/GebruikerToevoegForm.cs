5c45c2c9048fddf02a49be081e9ec4e527138c80
Pigna <myron.antonissen@gmail.com>
2015-04-17 10:34:08 +0200

    Aanpassing Gebruikertoevoegform dataophalen

--- ICT4Events_Group1/ICT4Events_Group1/GebruikerToevoegForm.cs
+++ ICT4Events_Group1/ICT4Events_Group1/GebruikerToevoegForm.cs
@@ -19,7 +19,45 @@ namespace ICT4Events_Group1
 
         private void btnAanmaken_Click(object sender, EventArgs e)
         {
-            
+            string voornaam = tbVoornaam.Text;
+            string tussenvoegsel = tbTussenvoegsel.Text; //niet verplicht
+            string achternaam = tbAchternaam.Text;
+            string straat = tbStraat.Text;
+            string huisnummer = tbHuisnummer.Text;
+            string postcode = tbPostcode.Text;
+            string woonplaats = tbWoonplaats.Text;
+            string rekeningnr = tbRekeningnr.Text;
+            string email = tbEmail.Text;
+            string vastnr = tbVastnr.Text;
+            string mobielnr = tbMobielnr.Text;
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+            string passwordcheck = tbPasswordCheck.Text;
+            //Check of verplichte velden niet leeg zijn.
+            if
+            (
+            voornaam != "" &
+            achternaam != "" &
+            straat != "" &
+            huisnummer != "" &
+            postcode != "" &
+            woonplaats != "" &
+            rekeningnr != "" &
+            email != "" &
+            username != "" &
+            password != "" &
+            passwordcheck != ""
+            )
+            {
+                //check password en email en of gebruiersnaam al bestaad
+                //email check
+                
+                //User user = new User(1, );
+            }
+            else
+            {
+                MessageBox.Show("Vul alle verplichte velden in.");
+            }
         }
     }
 }
