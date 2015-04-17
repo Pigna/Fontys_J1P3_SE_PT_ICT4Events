8541e57b5deed9a12e251feb14de0af647ace589
MJJH <martijnvriens94@live.nl>
2015-04-17 14:54:52 +0200

    EventDatabase en Database Commit

--- ICT4Events_Group1/ICT4Events_Group1/Database/EventDatabase.cs
+++ ICT4Events_Group1/ICT4Events_Group1/Database/EventDatabase.cs
@@ -15,8 +15,8 @@ namespace ICT4Events_Group1
 
             List<Dictionary<string, object>> data = getQuery("SELECT Id, Username, Password, Adminrights FROM Employee");
 
-            for(int c = 0; c < ret.Count; c++)
-                ret.Add(new Employee((int)data[c]["id"], (string)data[c]["Username"], (string)data[c]["Password"], (int)data[c]["Adminrights"] == 1));
+            for(int c = 0; c < data.Count; c++)
+                ret.Add(new Employee(Convert.ToInt16(data[c]["id"]), (string)data[c]["username"], (string)data[c]["password"], Convert.ToInt16(data[c]["ddminrights"]) == 1));
 
             return ret;
         }
@@ -27,22 +27,20 @@ namespace ICT4Events_Group1
 
             List<Dictionary<string, object>> data = getQuery("SELECT Id, Username, Password, Adminrights FROM Employee WHERE Username LIKE '%" + filter + "%' OR Id LIKE '%" + filter + "%';");
 
-            for (int c = 0; c < ret.Count; c++)
-                ret.Add(new Employee((int)data[c]["id"], (string)data[c]["Username"], (string)data[c]["Password"], (int)data[c]["Adminrights"] == 1));
+            for (int c = 0; c < data.Count; c++)
+                ret.Add(new Employee(Convert.ToInt16(data[c]["id"]), (string)data[c]["username"], (string)data[c]["password"], Convert.ToInt16(data[c]["adminrights"]) == 1));
 
             return ret;
         }
 
         public bool deleteEmployee(Employee e)
         {
-            //return doQuery("DELETE FROM Employee WHERE Id = " + e.id).Count > 0;
-            return false;
+            return doQuery("DELETE FROM Employee WHERE Id = " + e.id) > 0;
         }
 
         public bool createEmployee(Employee e)
         {
-            //return doQuery("INSERT INTO Employee (Id, Username, Password) VALUES (" + e.id + ", " + e.username + ", " + e.password + ");").Count > 0;
-            return false;
+            return doQuery("INSERT INTO Employee (Id, Username, Password) VALUES (" + e.id + ", " + e.username + ", " + e.password + ");") > 0;
         }
 
 
@@ -50,38 +48,34 @@ namespace ICT4Events_Group1
         {
             List<Event> ret = new List<Event>();
 
-            List<Dictionary<string, object>> data = getQuery("SELECT Id, Naam, Startdatum, Einddatum, Beschrijving, Kosten FROM Employee");
+            List<Dictionary<string, object>> data = getQuery("SELECT Id, Naam, Startdatum, Einddatum, Beschrijving, Kosten FROM event");
 
-            for (int c = 0; c < ret.Count; c++)
-                ret.Add(new Event((int)data[c]["id"], (string)data[c]["Naam"], (string)data[c]["Beschrijving"], (DateTime)data[c]["STARTDATUM"], (DateTime)data[c]["EINDDATUM"], (float) data[c]["Kosten"]));
+            for (int c = 0; c < data.Count; c++)
+                ret.Add(new Event(Convert.ToInt16(data[c]["id"]), (string)data[c]["naam"], (string)data[c]["beschrijving"], (DateTime)data[c]["startdatum"], (DateTime)data[c]["einddatum"], (float) data[c]["kosten"]));
             
             return ret;
         }
 
         public List<Event> getEvents(string filter)
         {
-            /*List<Event> ret = new List<Event>();
+            List<Event> ret = new List<Event>();
 
-            OracleDataReader data = getQuery("SELECT Id, Naam, Startdatum, Einddatum, Beschrijving, Kosten FROM Employee WHERE Naam LIKE '%"+filter+"%' OR Beschrijving LIKE '%"+filter+"%' OR Id LIKE '%"+filter+"%'");
+            List<Dictionary<string, object>> data = getQuery("SELECT Id, Naam, Startdatum, Einddatum, Beschrijving, Kosten FROM Employee WHERE Naam LIKE '%"+filter+"%' OR Beschrijving LIKE '%"+filter+"%' OR Id LIKE '%"+filter+"%'");
 
-            while (data.Read())
-            {
-                ret.Add(new Event(data.GetInt16(0), data.GetString(1), data.GetString(4), data.GetDateTime(2), data.GetDateTime(2), (float)data.GetDecimal(5)));
-            }
-            return ret;*/
-            return null;
+            for (int c = 0; c < data.Count; c++)
+                ret.Add(new Event(Convert.ToInt16(data[c]["id"]), (string)data[c]["naam"], (string)data[c]["beschrijving"], (DateTime)data[c]["startdatum"], (DateTime)data[c]["einddatum"], (float)data[c]["kosten"]));
+
+            return ret;
         }
 
         public bool deleteEvent(Event e)
         {
-            return false;
-            //return doQuery("DELETE FROM Event WHERE Id = " + e.id).Count > 0;
+            return doQuery("DELETE FROM Event WHERE Id = " + e.id) > 0;
         }
 
         public bool createEvent(Event e)
         {
-            return false;
-            //return doQuery("INSERT INTO Event (Id, Naam, Startdatum, Einddatum, Beschrijving, Kosten) VALUES (" + e.id + ", '" + e.Name + "', to_date('" + e.startDate + "','MM-DD-YYYY hh24:MI'),  to_date('" + e.endDate + "','MM-DD-YYYY hh24:MI'), "+e.price+")").Count > 0;
+            return doQuery("INSERT INTO Event (Id, Naam, Startdatum, Einddatum, Beschrijving, Kosten) VALUES (" + e.id + ", '" + e.Name + "', to_date('" + e.startDate + "','MM-DD-YYYY hh24:MI'),  to_date('" + e.endDate + "','MM-DD-YYYY hh24:MI'), "+e.price+")") > 0;
         }
     }
 }
