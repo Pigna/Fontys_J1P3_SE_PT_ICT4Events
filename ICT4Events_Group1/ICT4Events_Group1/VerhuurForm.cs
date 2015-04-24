982af9e8be2e54bff6898d2ab1fba929c839cc9d
kevin1995deman <kevin1995deman@gmail.com>
2015-04-24 12:00:10 +0100

    stuff

--- ICT4Events_Group1/ICT4Events_Group1/VerhuurForm.cs
+++ ICT4Events_Group1/ICT4Events_Group1/VerhuurForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Phidgets.Events;
+using Phidgets;
 
 namespace ICT4Events_Group1
 {
@@ -14,21 +16,33 @@ namespace ICT4Events_Group1
     {
         VerhuurDatabase db = new VerhuurDatabase();
         string cat = null;
+        RFID rfid = new RFID();
 
         public VerhuurForm()
         {
             InitializeComponent();
+           
+
+            rfid.Attach += new AttachEventHandler(rfid_Attach);
+            rfid.Detach += new DetachEventHandler(rfid_Detach);
+
+
+            rfid.Tag += new TagEventHandler(rfid_Tag);
+            rfid.TagLost += new TagEventHandler(rfid_TagLost);
+
+            openCmdLine(rfid);
 
             printList(db.getCatAantal(VerhuurDatabase.Soort.Alles));
         }
 
-        private void printList(Dictionary<string, int> list){
+        private void printList(Dictionary<string, int> list)
+        {
             lbxRentList.Items.Clear();
 
-            foreach(KeyValuePair<string, int> entry in list)
-                {
-                    lbxRentList.Items.Add(entry.Key + " (" + entry.Value + ")");
-                }
+            foreach (KeyValuePair<string, int> entry in list)
+            {
+                lbxRentList.Items.Add(entry.Key + " (" + entry.Value + ")");
+            }
         }
 
         private void VerhuurForm_Load(object sender, EventArgs e)
@@ -36,7 +50,7 @@ namespace ICT4Events_Group1
 
         }
 
-   
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -46,7 +60,7 @@ namespace ICT4Events_Group1
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            if(cat == null) 
+            if (cat == null)
                 printList(db.getCatAantal(getSoort(), tbxSearch.Text));
             else
             {
@@ -67,18 +81,18 @@ namespace ICT4Events_Group1
 
         private void tbxItemID_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-        
+
         }
 
         private void radVerhuurd_CheckedChanged(object sender, EventArgs e)
         {
-            if(cat == null) printList(db.getCatAantal(getSoort()));
-            else 
+            if (cat == null) printList(db.getCatAantal(getSoort()));
+            else
             {
                 lbxRentList.Items.Clear();
                 lbxRentList.Items.AddRange(db.getItems(cat, getSoort()).ToArray());
@@ -87,7 +101,7 @@ namespace ICT4Events_Group1
 
         private void radAlles_CheckedChanged(object sender, EventArgs e)
         {
-            if(cat == null) printList(db.getCatAantal(getSoort()));
+            if (cat == null) printList(db.getCatAantal(getSoort()));
             else
             {
                 lbxRentList.Items.Clear();
@@ -97,7 +111,7 @@ namespace ICT4Events_Group1
 
         private void radHuurbaar_CheckedChanged(object sender, EventArgs e)
         {
-            if(cat == null) printList(db.getCatAantal(getSoort()));
+            if (cat == null) printList(db.getCatAantal(getSoort()));
             else
             {
                 lbxRentList.Items.Clear();
@@ -129,14 +143,152 @@ namespace ICT4Events_Group1
 
         private void btnRentItem_Click(object sender, EventArgs e)
         {
-            if (!db.addVerhuur(Convert.ToInt16(tbxItemID.Text), Convert.ToInt16(txtGebruiker.Text), dtpVan.Value, dtpTot.Value))
-                MessageBox.Show("n");
+            if (tbxRFID.Text == "" || tbxItemID.Text == "")
+            {
+                MessageBox.Show("niet alle gegevens zijn ingevult!");
+            }
+            else
+            {
+                try
+                {
+                    int itemID = Convert.ToInt32(tbxItemID.Text);
+                    if (db.checkrfid(tbxRFID.Text))
+                    {
+                        MessageBox.Show("rfid is niet gelinkt aan een gebruiker");
+                    }
+
+                    else if (!db.addVerhuur(Convert.ToInt16(itemID), tbxRFID.Text, dtpVan.Value, dtpTot.Value))
+                        MessageBox.Show("Item is al uitgeleend");
+                    else
+                        MessageBox.Show("Item is op rfid gezet");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.ToString());
+                    MessageBox.Show("de ingevulde gegevens zijn niet correct");
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(!db.brengTerug(Convert.ToInt16(txtItem.Text), ckbBetaald_b.Checked))
+            if (!db.brengTerug(Convert.ToInt16(txtItem.Text), ckbBetaald_b.Checked))
                 MessageBox.Show("n");
         }
+
+
+
+
+        // als de tag gescant is wordt het veld gevult met de code
+        void rfid_Tag(object sender, TagEventArgs e)
+        {
+            tbxRFID.Text = e.Tag;
+
+
+
+        }
+
+        //als de tag weg is word het veld weer blank
+        void rfid_TagLost(object sender, TagEventArgs e)
+        {
+            tbxRFID.Text = "";
+
+        }
+        void rfid_Attach(object sender, AttachEventArgs e)
+        {
+            rfid.Antenna = true;
+        }
+
+        void rfid_Detach(object sender, DetachEventArgs e)
+        {
+            rfid.Antenna = false;
+        }
+
+
+
+        #region Command line open functions
+        private void openCmdLine(Phidget p)
+        {
+            openCmdLine(p, null);
+        }
+        private void openCmdLine(Phidget p, String pass)
+        {
+            int serial = -1;
+            String logFile = null;
+            int port = 5001;
+            String host = null;
+            bool remote = false, remoteIP = false;
+            string[] args = Environment.GetCommandLineArgs();
+            String appName = args[0];
+
+            try
+            { //Parse the flags
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith("-"))
+                        switch (args[i].Remove(0, 1).ToLower())
+                        {
+                            case "l":
+                                logFile = (args[++i]);
+                                break;
+                            case "n":
+                                serial = int.Parse(args[++i]);
+                                break;
+                            case "r":
+                                remote = true;
+                                break;
+                            case "s":
+                                remote = true;
+                                host = args[++i];
+                                break;
+                            case "p":
+                                pass = args[++i];
+                                break;
+                            case "i":
+                                remoteIP = true;
+                                host = args[++i];
+                                if (host.Contains(":"))
+                                {
+                                    port = int.Parse(host.Split(':')[1]);
+                                    host = host.Split(':')[0];
+                                }
+                                break;
+                            default:
+                                goto usage;
+                        }
+                    else
+                        goto usage;
+                }
+                if (logFile != null)
+                    Phidget.enableLogging(Phidget.LogLevel.PHIDGET_LOG_INFO, logFile);
+                if (remoteIP)
+                    p.open(serial, host, port, pass);
+                else if (remote)
+                    p.open(serial, host, pass);
+                else
+                    p.open(serial);
+                return; //success
+            }
+            catch { }
+        usage:
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Invalid Command line arguments." + Environment.NewLine);
+            sb.AppendLine("Usage: " + appName + " [Flags...]");
+            sb.AppendLine("Flags:\t-n   serialNumber\tSerial Number, omit for any serial");
+            sb.AppendLine("\t-l   logFile\tEnable phidget21 logging to logFile.");
+            sb.AppendLine("\t-r\t\tOpen remotely");
+            sb.AppendLine("\t-s   serverID\tServer ID, omit for any server");
+            sb.AppendLine("\t-i   ipAddress:port\tIp Address and Port. Port is optional, defaults to 5001");
+            sb.AppendLine("\t-p   password\tPassword, omit for no password" + Environment.NewLine);
+            sb.AppendLine("Examples: ");
+            sb.AppendLine(appName + " -n 50098");
+            sb.AppendLine(appName + " -r");
+            sb.AppendLine(appName + " -s myphidgetserver");
+            sb.AppendLine(appName + " -n 45670 -i 127.0.0.1:5001 -p paswrd");
+            MessageBox.Show(sb.ToString(), "Argument Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            Application.Exit();
+        }
+        #endregion
     }
 }
